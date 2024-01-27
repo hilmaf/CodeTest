7303f8e9eaf355fb942ef3d15c3879de8a5ee557
orlando <glee1470@naver.com>
2024-01-27 15:01:32 +0900

    [chore] 주석 추가

--- basic/string_10/src/Main.java
+++ basic/string_10/src/Main.java
@@ -1,8 +1,7 @@
 import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
-
+        // 10번
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
         char c = str.charAt(str.length()-1);
@@ -13,13 +12,14 @@ public class Main {
             System.out.print(x + " ");
         }
     }
-
     public int[] solution(String str, char c) {
+        // 기준이 되는 문자를 제외한 단어만 추출
         String word = str.substring(0, str.length()-2);
 
-        int p=101;
-        char[] wordArr = word.toCharArray();
-        int[] pArr = new int[word.length()];
+        int p=101; // 문자열의 길이는 100을 넘지 않는다
+        char[] wordArr = word.toCharArray(); // 단어 char 배열
+        int[] pArr = new int[word.length()]; // 거리값 배열
+
         for(int i=0; i<wordArr.length; i++) {
             if(wordArr[i] == c) {
                 p=0;
