b82401105e89a524390b876a556d7bc67a0da2f9
orlando <glee1470@naver.com>
2024-02-18 13:23:32 +0900

    Pointers_01 두 배열 합치기(투포인터 알고리즘 활용)

--- basic/pointers_01/src/Main.java
+++ basic/pointers_01/src/Main.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.stream.Stream;
@@ -9,28 +10,37 @@ public class Main {
         // 1번. 두 배열 합치기
         Scanner sc = new Scanner(System.in);
         int arr1Length = Integer.parseInt(sc.next());
-        Integer[] arr1 = new Integer[arr1Length];
+        int[] arr1 = new int[arr1Length];
         for (int i = 0; i < arr1Length; i++) {
             arr1[i] = Integer.parseInt(sc.next());
         }
 
         int arr2Length = Integer.parseInt(sc.next());
-        Integer[] arr2 = new Integer[arr2Length];
+        int[] arr2 = new int[arr2Length];
         for (int i = 0; i < arr2Length; i++) {
             arr2[i] = Integer.parseInt(sc.next());
         }
 
-        int catLength = arr1Length + arr2Length;
-        int[] catArr = Stream
-                .concat(Arrays.stream(arr1), Arrays.stream(arr2))
-                .mapToInt(Integer::intValue)
-                .toArray();
-
-        Arrays.sort(catArr);
-
-        for (int i = 0; i < catLength; i++) {
-            System.out.print(catArr[i] + " ");
+        int pnt1 = 0, pnt2 = 0;
+        ArrayList<Integer> answerList = new ArrayList<Integer>();
+        while(pnt1<arr1Length && pnt2<arr2Length) {
+            if(arr1[pnt1] <= arr2[pnt2]) {
+                answerList.add(arr1[pnt1++]);
+            } else {
+                answerList.add(arr2[pnt2++]);
+            }
         }
 
+        while(pnt1<arr1Length) {
+            answerList.add(arr1[pnt1++]);
+        }
+
+        while(pnt2<arr2Length) {
+            answerList.add(arr2[pnt2++]);
+        }
+
+        for(int x : answerList) {
+            System.out.print(x + " ");
+        }
     }
 }
