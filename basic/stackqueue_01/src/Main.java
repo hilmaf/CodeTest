43762be3343b100608405823399d71a99888b85a
orlando <glee1470@naver.com>
2024-03-11 00:54:09 +0900

    StackQueue 01. 올바른 괄호(스택 사용)

--- basic/stackqueue_01/src/Main.java
+++ basic/stackqueue_01/src/Main.java
@@ -1,28 +1,46 @@
+import java.time.temporal.ChronoField;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
         char[] strArr = str.toCharArray();
+
         String answer = "YES";
-        int open = 0, close=0;
+        Stack<Character> stack = new Stack<Character>();
+
+//        stack.push(strArr[0]);
+
         for (int i = 0; i < strArr.length; i++) {
-            if(open==close && strArr[i] == ')') {
-                answer="NO";
-                break;
+
+            if(strArr[i] == '(') {
+                stack.push(strArr[i]);
+            } else if(strArr[i] == ')') {
+                if(!stack.isEmpty() && stack.peek() == '(') {
+                    stack.pop();
+                } else {
+                    answer = "NO";
+                    break;
+                }
+//                stack.push(!stack.isEmpty() && stack.peek() == '(' ? stack.pop() : strArr[i]);
             }
-
-            switch(strArr[i]) {
-                case '(': open++; break;
-                case ')': close++; break;
-            }
-
+            
         }
+        
+//        for (int i = 1; i < strArr.length; i++) {
+//            stack.push(strArr[i]);
+//
+//            if(stack.elementAt(0) == '(' && stack.peek() == ')') {
+//                stack.pop();
+//                stack.pop();
+//            }
+//        }
 
-        if(open != close) {
-            answer = "NO";
-        }
+//        if(!stack.isEmpty()) {
+//            answer = "NO";
+//        }
 
         System.out.println(answer);
 
