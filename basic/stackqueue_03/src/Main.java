5d7be00f60f599d5c78db2239a9ddc1e2b2e4183
orlando <glee1470@naver.com>
2024-03-13 20:28:41 +0900

    StackQueue 03. 크레인 인형뽑기(카카오)

---
+++ basic/stackqueue_03/src/Main.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int boardSize = sc.nextInt();
+        int[][] board = new int[boardSize][boardSize];
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++) {
+                board[i][j] = sc.nextInt();
+            }
+        }
+
+        int movesSize = sc.nextInt();
+        int[] moves = new int[movesSize];
+        for (int i = 0; i < movesSize; i++) {
+            moves[i] = sc.nextInt();
+        }
+
+        //////////////////////////////////////////
+
+        Stack<Integer> stack = new Stack<>();
+        int answer = 0;
+
+        for (int i = 0; i < movesSize; i++) {
+            int j = 0;
+
+            while(board[j][moves[i]-1]==0) {
+                if (j == boardSize-1) {
+                    break;
+                }
+                j++;
+            }
+
+
+            if(!stack.isEmpty() && stack.peek() == board[j][moves[i]-1]) {
+                board[j][moves[i]-1] = 0;
+                stack.pop();
+                answer += 2;
+            } else {
+                stack.push(board[j][moves[i]-1]);
+                board[j][moves[i]-1] = 0;
+            }
+        }
+
+        System.out.println(answer);
+
+    }
+}
